10b89b43b00d12c9c1188b473dfb9e8ff6b549c4
siangulo1101 <98188515+siangulo1101@users.noreply.github.com>
2022-03-12 09:12:25 -0500

    documents VSC

---
+++ styles.css
@@ -0,0 +1,100 @@
+* {
+    padding: 0px;
+    margin: 0px;
+}
+
+.contenedor {
+background: url(https://wallpapercave.com/wp/wp3492732.jpg);
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    justify-content: center;
+    align-items: center;
+}
+
+.cuadro {
+    width: 600px;
+    height: 600px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    gap: 10px;
+    background-color: black;
+}
+
+.cuadro div {
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: 80px;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.cuadro div:hover {
+    background-color: rgba(218,218,218,1);
+    cursor: pointer;
+}
+
+button{
+      padding: 20px 10px;
+        background-color: #7428CC;
+        color: white;
+        outline:none;
+        border: 0px;
+        border-radius: 8px;
+        width: 200px;
+        margin-top: 20px;
+        font-weight: bold;
+        font-size: 18px;
+        font-family: 'Courier New', Courier, monospace;
+        letter-spacing: 1px;
+    }
+
+    button:hover {
+        cursor: pointer;
+        background-color: #6320af;
+   }
+
+.lineContainer {
+    position: relative;
+}
+
+#linea{
+    position: absolute;
+    display: block;
+    background-color: #34283f;
+}
+
+.turnos{
+    display: grid;
+    gap: 10px;
+    grid-template-columns: 1fr 1fr;
+    margin-bottom: 20px;
+}
+
+#X{
+    padding: 20px 10px;
+    background-color: BEB3B6;
+    border-radius: 4px;
+    width: 200px;
+    font-size: 20px;
+    font-family: 'Courier New', Courier, monospace;
+    text-align: center;
+    letter-spacing: 1px;
+    font-style: bold;
+}
+
+#O{
+    padding: 20px 10px;
+    background-color: BEB3B6;
+    border-radius: 4px;
+    width: 200px;
+    font-size: 20px;
+    font-family: 'Courier New', Courier, monospace;
+    text-align: center;
+    letter-spacing: 1px;
+    font-style: bold;
+  }
